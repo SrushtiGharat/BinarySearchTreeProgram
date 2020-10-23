bc7c1d23bdca520d0d6d30817120bc607aa27405
Srushti <Srushti@DESKTOP-QP0QMA4>
2020-10-23 23:43:31 +0530

    [Srushti] Add.Create binary tree and add data

---
+++ BinarySearchTreeProgram/BinaryTree.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BinarySearchTreeProgram
+{
+    class BinarySearchTree<T> where T : IComparable
+    {
+        Node<T> Root;
+        Node<T> Current;
+        
+        public void InsertData(T data)
+        {
+            if (Root == null)
+            {
+                this.Root = new Node<T>(data);
+                this.Current = Root;
+                return;
+            }
+            if (this.Current.NodeValue.CompareTo(data) > 0)
+            {
+                if (this.Current.LeftNode == null)
+                {
+                    this.Current.LeftNode = new Node<T>(data);
+                    this.Current = Root;
+
+                }
+                else
+                {
+                    this.Current = this.Current.LeftNode;
+                    InsertData(data);
+                }
+            }
+            else
+            {
+                if (this.Current.RightNode == null)
+                {
+                    this.Current.RightNode = new Node<T>(data);
+                    this.Current = Root;
+                }
+                else
+                {
+                    this.Current = this.Current.RightNode;
+                    InsertData(data);
+                }
+            }
+
+        }
+        public Node<T> GetRoot()
+        {
+            return this.Root;
+        }
+        public void Display(Node<T> node)
+        {
+            if (node != null)
+            {
+                Display(node.LeftNode);
+                Console.WriteLine(" " + node.NodeValue);
+                Display(node.RightNode);
+            }
+
+        }
+    }
+  }
--- BinarySearchTreeProgram/Program.cs
+++ BinarySearchTreeProgram/Program.cs
@@ -7,6 +7,13 @@ namespace BinarySearchTreeProgram
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Binary Search Tree Program");
+            Console.WriteLine("Welcome To Binary Search Tree Program");
+            BinarySearchTree<int> binarySearchTree = new BinarySearchTree<int>();
+            binarySearchTree.InsertData(56);
+            binarySearchTree.InsertData(30);
+            binarySearchTree.InsertData(70);
+
+            binarySearchTree.Display(binarySearchTree.GetRoot());
         }
     }
 }
